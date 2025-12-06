35780ce23340acf002cd534150912b0eedd153c5
Artem23112 <artemchernichuk@gmail.com>
2025-12-06 19:21:06 +0200

    fixed: how buttons become active

--- src/components/ui/buttons/add-to-cart/add-to-cart.tsx
+++ src/components/ui/buttons/add-to-cart/add-to-cart.tsx
@@ -3,16 +3,18 @@ import type { FC } from 'react';
 import s from './add-to-cart.module.scss';
 
 type Props = {
+  isActive: boolean;
   children?: React.ReactNode;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 export const AddToCart: FC<Props> = ({
   className = '',
   children = 'Add to cart',
+  isActive,
   ...attr
 }) => {
   return (
-    <button className={clsx(s.btn, className)} {...attr}>
+    <button className={clsx(s.btn, className, isActive && s.active)} {...attr}>
       {children}
     </button>
   );
--- src/components/ui/buttons/add-to-wishlist/add-to-wishlist.tsx
+++ src/components/ui/buttons/add-to-wishlist/add-to-wishlist.tsx
@@ -2,11 +2,17 @@ import clsx from 'clsx';
 import type { FC } from 'react';
 import s from './add-to-wishlist.module.scss';
 
-type Props = {} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+type Props = {
+  isActive: boolean;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-export const AddToWishlist: FC<Props> = ({ className = '', ...attr }) => {
+export const AddToWishlist: FC<Props> = ({
+  className = '',
+  isActive,
+  ...attr
+}) => {
   return (
-    <button className={clsx(s.btn, className)} {...attr}>
+    <button className={clsx(s.btn, className, isActive && s.active)} {...attr}>
       ❤️
     </button>
   );
