852298b267884c2a51dbe5dfce6636c2dcec13c9
artem23112 <artemchernichuk@gmail.com>
2026-02-02 23:18:16 +0200

    Add empty cart state to cart page

--- src/pages/cart-page/cart-page.tsx
+++ src/pages/cart-page/cart-page.tsx
@@ -1,3 +1,27 @@
+import { useAppSelector } from '@app/rtk/hooks/hooks';
+import { ProductList } from '@components/product-list/product-list';
+import { ProductListSkeleton } from '@components/product-list/product-list-skeleton';
+import { cartItemsSelector } from '@features/cart/cartSlice';
+import { useGetAllProductsQuery } from '@features/products/productsApi';
+import { SectionWithTitle } from '@layout/section-with-title/section-with-title';
+import s from './cart-page.module.scss';
+
 export const CartPage = () => {
-  return <div>cart</div>;
+  const cartItems = useAppSelector(cartItemsSelector);
+  const { data: allProducts = [], isLoading } = useGetAllProductsQuery();
+  // Bad way to receive all items from server, but server doesn't have endpoint with string of ids parameter
+  const cartProducts = allProducts?.filter((product) =>
+    cartItems.includes(product.id)
+  );
+
+  return (
+    <SectionWithTitle title="Your Cart">
+      {isLoading && <ProductListSkeleton countItems={4} view="list" />}
+      {cartProducts.length > 0 ? (
+        <ProductList itemsView={'list'} products={cartProducts} />
+      ) : (
+        <p className={s.emptyCart}>Sorry, seems like your cart is empty</p>
+      )}
+    </SectionWithTitle>
+  );
 };
