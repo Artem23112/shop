6d2d52ba52255d0c39be6456abb4e4e87d2e8d98
artem23112 <artemchernichuk@gmail.com>
2026-02-02 22:38:08 +0200

    Add empty wishlist state styling and message

--- src/pages/wishlist-page/wishlist-page.tsx
+++ src/pages/wishlist-page/wishlist-page.tsx
@@ -4,6 +4,7 @@ import { ProductList } from '@components/product-list/product-list';
 import { ProductListSkeleton } from '@components/product-list/product-list-skeleton';
 import { useGetAllProductsQuery } from '@features/products/productsApi';
 import { SectionWithTitle } from '@layout/section-with-title/section-with-title';
+import s from './wishlist-page.module.scss';
 
 export const WishlistPage = () => {
   const wishlistIds = useAppSelector(
@@ -17,10 +18,13 @@ export const WishlistPage = () => {
 
   return (
     <SectionWithTitle title="Your Wishlist">
-      {isLoading ? (
-        <ProductListSkeleton countItems={4} view="list" />
-      ) : (
+      {isLoading && <ProductListSkeleton countItems={4} view="list" />}
+      {wishlistProducts.length > 0 ? (
         <ProductList itemsView={'list'} products={wishlistProducts} />
+      ) : (
+        <p className={s.emptyWishlist}>
+          Sorry, seems like your wishlist is empty
+        </p>
       )}
     </SectionWithTitle>
   );
